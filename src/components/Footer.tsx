34bbc9bd0c7e492c562c937fb81eb69b14896b5e
cheungchunman <cheung1993@gmail.com>
2021-05-09 20:54:10 +0800

    HelpUrls

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,6 +13,7 @@ const footerElements = [
   { description: 'Discord', link: helpUrls.discord },
   { description: 'Telegram', link: helpUrls.telegram },
   { description: 'GitHub', link: helpUrls.github },
+  { description: 'Twitter', link: helpUrls.twitter },
   { description: 'Project Serum', link: helpUrls.projectSerum },
   { description: 'Solana Network', link: helpUrls.solanaBeach },
 ];
--- src/components/HelpUrls.ts
+++ src/components/HelpUrls.ts
@@ -4,8 +4,9 @@ export const helpUrls = {
   contactEmail: 'mailto:samoyedloverbuysamoyed@gmail.com',
   discord: 'https://discord.gg/mD38zFCdYH',
   telegram: 'https://t.me/samoyedlovers',
-  github: 'https://github.com/project-serum',
+  github: 'https://github.com/codeislaw1993/serum-dex-ui',
   projectSerum: 'https://projectserum.com/',
   developerResources: 'https://projectserum.com/developer-resources',
   solanaBeach: 'https://solanabeach.io',
+  twitter: 'https://twitter.com/lover_samoyed',
 };
